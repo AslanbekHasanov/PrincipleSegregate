9bc20d47e77f6b4ecbe30f60446a496ad0b4dd30
AslanbekHasanov <learneraslanbek1220@gmail.com>
2024-03-25 01:08:27 +0500

    Add File Header

--- Principle Segregate/DigitalOcean.cs
+++ Principle Segregate/DigitalOcean.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//----------------------------------------
+// Tarteeb School (c) All rights reserved
+//----------------------------------------
+
+using System;
 
 namespace PrincipleSegregate
 {
